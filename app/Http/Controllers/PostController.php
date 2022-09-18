7606822c9d383d2b14ea5ccc03b2581ea7e120c6
m_maj <mmajnouni@gmail.com>
2022-09-18 16:17:27 +0430

    adding form and js files

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,25 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function index() {
+        return view('admin.posts.index');
+    }
     public function show(Post $post) {
         return view('blog-post', ['post' => $post]);
     }
     public function create(){
         return view('admin.posts.create');
+    }
+    public function store() {
+       $inputs = request()->validate([
+           'title' => 'required|min:4|max:255',
+           'post_image'  => 'file',
+           'body'  => 'required'
+       ]);
+       if (request('post_image')) {
+           $inputs['post_image'] = request('post_image')->store('images');
+       }
+       auth()->user()->posts()->create($inputs);
+        return back();
     }
 }
